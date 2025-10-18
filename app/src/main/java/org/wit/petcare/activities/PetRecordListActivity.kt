b4c08f2326ff65677488b15475038b535a841797
ChrisKelleher1947 <ckelleher1312@gmail.com>
2025-10-18 08:58:39 +0100

    Added Recycler View for Pet Record List

--- app/src/main/java/org/wit/petcare/activities/PetRecordListActivity.kt
+++ app/src/main/java/org/wit/petcare/activities/PetRecordListActivity.kt
@@ -2,16 +2,56 @@ package org.wit.petcare.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.wit.petcare.R
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import org.wit.petcare.databinding.ActivityPetRecordListBinding
+import org.wit.petcare.databinding.CardPetRecordBinding
 import org.wit.petcare.main.MainApp
+import org.wit.petcare.models.PetCareModel
 
-class PetRecordListActivity : AppCompatActivity() {
+class PlacemarkListActivity : AppCompatActivity() {
 
     lateinit var app: MainApp
+    private lateinit var binding: ActivityPetRecordListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pet_record_list)
+        binding = ActivityPetRecordListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         app = application as MainApp
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = PetcareAdapter(app.petRecords)
+    }
+}
+
+class PetcareAdapter constructor(private var petrecords: List<PetCareModel>) :
+    RecyclerView.Adapter<PetcareAdapter.MainHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        val binding = CardPetRecordBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return MainHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val petrecord = petrecords[holder.adapterPosition]
+        holder.bind(petrecord)
+    }
+
+    override fun getItemCount(): Int = petrecords.size
+
+    class MainHolder(private val binding : CardPetRecordBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(petrecord: PetCareModel) {
+            binding.petcareTitle.text = petrecord.petName
+            binding.description.text = petrecord.petType
+        }
     }
 }
